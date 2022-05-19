b9318aab85a091c69670625cea6a7f785bbc5b26
kingshuk.das@databricks.com <kingshuk.das@databricks.com>
2022-05-19 14:58:26 +0000

    This is for test

---
+++ Notebook1.sql
@@ -0,0 +1,17 @@
+-- Databricks notebook source
+DROP TABLE IF EXISTS diamonds;
+
+CREATE TABLE diamonds
+USING csv
+OPTIONS (path "/databricks-datasets/Rdatasets/data-001/csv/ggplot2/diamonds.csv", header "true")
+
+-- COMMAND ----------
+
+select
+  *
+from
+  database1.babynames_csv;
+
+-- COMMAND ----------
+
+CREATE DATABASE database1;
